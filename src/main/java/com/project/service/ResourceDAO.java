5425133bba9f16fa0ae442e0f4255f3cfc978a76
meta-vivek-mittal <vivek.mittal@metacube.com>
2016-10-19 18:26:48 +0530

    Add files via upload

--- src/main/java/com/project/service/ResourceDAO.java
+++ src/main/java/com/project/service/ResourceDAO.java
@@ -7,6 +7,7 @@ package com.project.service;
 
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
@@ -39,139 +40,224 @@ public class ResourceDAO {
 	/**
 	 * Following function fires query to database to get the required result(i.e
 	 * get all the available resources)
+	 * 
 	 * @return the list of all available resources .
 	 */
 	@SuppressWarnings("unchecked")
 	public List<ResourcesModel> allResourceList() {
 
-		Session session = this.sessionFactory.getCurrentSession();
+		Session session = sessionFactory.openSession();
 
-	
 		// Getting the result
 		return session.createCriteria(ResourcesModel.class).list();
-		
+
 	}
 
-
 	/**
-	 * Following function helps in creating a new resource. 
-	 * @param resourcesModel contains the information of the new resource.
+	 * Following function helps in creating a new resource.
+	 * 
+	 * @param resourcesModel
+	 *            contains the information of the new resource.
 	 * @return true/false whether resource created successfully or not.
 	 */
 	public boolean createResource(ResourcesModel resourceModel) {
-		//Creating a new session
-				Session session = sessionFactory.openSession();
-				
-				try {
-					//Starting a new transaction
-					session.beginTransaction();
-					
-					//Inserting the Users Details in the database
-					session.save(resourceModel);
-					
-					//Committing the current transaction
-					session.getTransaction().commit();
-					
-					return true;
-				} catch (Exception e) {
-					session.getTransaction().rollback();
-					return false;
-				}
-	}
-
-	/**
-	 * Following function helps in deleting an existing resource. 
-	 * @param resourcesModel contains the information of resource to be deleted.
-	 * @return true/false whether resource deleted successfully or not.
-	 */
-	/*public boolean deleteResource(ResourcesModel resourceModel) {
-		//Creating a new session
+		// Creating a new session
 		Session session = sessionFactory.openSession();
-		
+
 		try {
-			//Starting a new transaction
+			// Starting a new transaction
 			session.beginTransaction();
-			
-			//Inserting the Users Details in the database
-			session.delete(resourceModel);
-			
-			//Committing the current transaction
+
+			// Inserting the Users Details in the database
+			session.save(resourceModel);
+
+			// Committing the current transaction
 			session.getTransaction().commit();
-			
+
 			return true;
 		} catch (Exception e) {
 			session.getTransaction().rollback();
 			return false;
 		}
-	}*/
+	}
 
 	/**
-	 * Following function helps in updating the existing resource. 
-	 * @param resourcesModel contains the information of the resource to be updated.
+	 * Following function helps in deleting an existing resource.
+	 * 
+	 * @param resourcesModel
+	 *            contains the information of resource to be deleted.
+	 * @return true/false whether resource deleted successfully or not.
+	 */
+	/*
+	 * public boolean deleteResource(ResourcesModel resourceModel) { //Creating
+	 * a new session Session session = sessionFactory.openSession();
+	 * 
+	 * try { //Starting a new transaction session.beginTransaction();
+	 * 
+	 * //Inserting the Users Details in the database
+	 * session.delete(resourceModel);
+	 * 
+	 * //Committing the current transaction session.getTransaction().commit();
+	 * 
+	 * return true; } catch (Exception e) { session.getTransaction().rollback();
+	 * return false; } }
+	 */
+
+	/**
+	 * Following function helps in updating the existing resource.
+	 * 
+	 * @param resourcesModel
+	 *            contains the information of the resource to be updated.
 	 * @return true/false whether resource updated successfully or not.
 	 */
 	public boolean editResource(ResourcesModel resourceModel) {
-		//Creating a new session
-				Session session = sessionFactory.openSession();
-				
-				try {
-					//Starting a new transaction
-					session.beginTransaction();
-					
-					//Inserting the Users Details in the database
-					session.update(resourceModel);
-					
-					//Committing the current transaction
-					session.getTransaction().commit();
-					
-					return true;
-				} catch (Exception e) {
-					session.getTransaction().rollback();
-					return false;
-				}
-	}
 
-	public boolean createResourceAdmin(ResourcesModel resourceModel,
-			UsersModel usersModel) {
-		
+		// Creating a new session
 		Session session = sessionFactory.openSession();
 
-		
-		try{
-			//Starting a new transaction
+		try {
+			// Starting a new transaction
 			session.beginTransaction();
-			
-			
-			
-			ResourcesModel updateResource = (ResourcesModel) session.get(ResourcesModel.class, resourceModel.getResourceId());
-			List<UsersModel> resourceAdmins = updateResource.getResourceAdmins();
-			resourceAdmins.add(usersModel);
-			
-			//DEBUG
-			updateResource.setResourceAdmins(resourceAdmins);
-			
-			/*UsersModel updateUsers = (UsersModel) session.get(UsersModel.class, usersModel.getEmail());
-			List<ResourcesModel> userAsAdminForResources = updateUsers.getAdminOfResources();
-			userAsAdminForResources.add(resourceModel);
-			*/
-			//DEBUG
-			/*updateUsers.setAdminOfResources(userAsAdminForResources);*/
-			
-			System.out.println("started 2");
-			//DEBUG
-			session.getTransaction().commit();
-			
-			System.out.println("End");	
-			//DEBUG
-			return true;
-			
-		} catch (Exception e) 
-			{
-			session.getTransaction().rollback();
-			
-			return false;
+
+			// fetching user list from resource model;
+			// Users who are resource_admins to this particular resource
+			List<UsersModel> usersModel = (List<UsersModel>) resourceModel
+					.getResourceAdmins();
+
+			// getting resourceModel from DataBase
+			ResourcesModel resourcesModelDataBase = (ResourcesModel) session
+					.get(ResourcesModel.class, resourceModel.getResourceId());
+
+			// user update
+			// if resource have any resource_admin
+			// list of users to be assign as res_admin for this resource
+			if (usersModel.size() != 0) {
+
+				System.out.println("begin"); // DEBUG
+				System.out.println("resourceMdel  " + resourceModel); // DEBUG
+				System.out.println("getting users and " + usersModel); // DEBUG
+				System.out.println("updating resourceModel"); // DEBUG
+				System.out.println("Size of usersmodel " + usersModel.size()); // DEBUG
+
+				List<ResourcesModel> adminOfResources;
+
+				// getting individual resource admin
+				for (UsersModel usersModel2 : usersModel) {
+
+					System.out.println("updating resourceModel 1");
+
+					System.out.println("updating resourceModel 2");
+
+					// getting res_admin from database by employeeId
+					UsersModel usersModelDB = (UsersModel) session.get(
+							UsersModel.class, usersModel2.getEmployeeId());
+
+					System.out.println("updating resourceModel 3");
+
+					// checking for role
+					if (usersModelDB.getRole().equals("user"))
+						usersModelDB.setRole("res_admin");
+
+					System.out.println("updating resourceModel 4");
+
+					// getting resource for res_admin
+					adminOfResources = usersModelDB.getAdminOfResources();
+
+					// adding new resource to list for res_admin
+					boolean flag = true;
+					for (ResourcesModel resourcesModelLocal : adminOfResources) {
+
+						if (resourcesModelLocal.getResourceId() == resourceModel
+								.getResourceId()) {
+							flag = false;
+						}
+
+					}
+					// if resource is not in res_admin relation for that user
+					if (flag) {
+
+						adminOfResources.add(resourceModel);
+					}
+
+					// setting new list of resources for res_admin
+					usersModelDB.setAdminOfResources(adminOfResources);
+
+					// updating Object
+					session.update(usersModelDB);
+					System.out.println("updating resourceModel 6");
+					// session.update(usersModel2);
+
+				}
+
+			} else {
+
+				// getting res_admin from database for specific resource
+				List<UsersModel> usersModelList = resourcesModelDataBase
+						.getResourceAdmins();
+
+				System.out.println("users from database for resource "
+						+ usersModelList.size()); // DEBUG
+
+				List<ResourcesModel> adminOfResources;
+
+					/*
+					 * deleting resource from user Model in res_admin relation
+					 * */
+				for (UsersModel usersModelLocal : usersModelList) {
+
+					System.out.println("updating resourceModel 1");
+					
+					// getting userModel from database by emloyeeId
+					UsersModel usersModelDataBase = (UsersModel) session.get(
+							UsersModel.class, usersModelLocal.getEmployeeId());
+
+					System.out.println("updating resourceModel 2");
+					
+					//getting resources list for which user is admin
+					adminOfResources = usersModelDataBase.getAdminOfResources();
+
+					if (adminOfResources.size() <= 1) {
+
+						System.out.println("updating resourceModel 3");
+						usersModelDataBase.setRole("user");
+						System.out.println("updating resourceModel 4");
+
+					}
+
+					//removing resource from user list
+					adminOfResources.remove(resourceModel);
+					
+					usersModelDataBase.setAdminOfResources(adminOfResources);
+					
+					// updated list resources for user
+					session.update(usersModelDataBase);
+				}
+
 			}
-		
+			
+			
+			//updating users(admin) list of resource
+			resourcesModelDataBase.setResourceAdmins(resourceModel.getResourceAdmins());
+			resourcesModelDataBase.setCapacity(resourceModel.getCapacity());
+			resourcesModelDataBase.setResourceName(resourceModel.getResourceName());
+
+			// Resource Update
+
+			// Inserting the Users Details in the database
+			session.update(resourcesModelDataBase);
+
+			System.out.println("updating usersModel");
+
+			// Committing the current transaction
+			session.getTransaction().commit();
+			System.out.println("commit done");
+
+			return true;
+		} catch (Exception e) {
+			session.getTransaction().rollback();
+			System.out.println("getting error");
+			return false;
+		}
 	}
 
 }
