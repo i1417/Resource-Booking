dd6229c2656a148a07a4b8c10e148b14ec61497b
amitsharma1100 <sharma.amit1100@gmail.com>
2016-10-19 15:22:26 +0530

    Pending Requests accept and reject by admin

--- src/main/webapp/ui/js/sidebarApp.js
+++ src/main/webapp/ui/js/sidebarApp.js
@@ -1,7 +1,7 @@
 var sidebarApp = angular.module('sidebarApp', ['dataShareFactory', 'dataFactory']);
 
 sidebarApp.controller('sidebarCtrl', function($scope, $http, $window, userDetails, utilityFunctions) {
-    console.log("fsfs");
+  
     $scope.currentUser = userDetails.getCurrentUser();
     $scope.resources = {};
 
@@ -11,8 +11,7 @@ sidebarApp.controller('sidebarCtrl', function($scope, $http, $window, userDetail
     		$('#pendingRequest').hide();
     	}
     } else {
-        console.log("hello");
-        $http({
+           $http({
             method : 'GET',
             url : 'http://localhost:8080/Project-Authentication/resources/getAll',
             data : $scope.currentUser,
@@ -29,32 +28,13 @@ sidebarApp.controller('sidebarCtrl', function($scope, $http, $window, userDetail
             alert("Connection Error");
         });
     }
-
-    $scope.fetchBookings = function(resourceId) {
-		console.log(resourceId);
-		$scope.resources.resourceId = resourceId;
-		console.log($scope.resources);
-        $http({
-            method : 'POST',
-            url : 'http://localhost:8080/Project-Authentication/bookings/getPendingbookings',
-            data : $scope.resources,
-            headers : {'Content-Type': 'application/json'}
-        }).success(function(response) {
-            console.log(response);
-
-            //console.log(response.data);
-            // $scope.user.password = "";
-            // if(response.status == 200 ) {
-            // 	userDetails.setCurrentUser(response.data);
-            // 	$window.location.href = 'admin/index.html';
-            // } else {
-            // 	$scope.user.password = "";
-            // 	console.log(response.errorMessage);
-            // }
-        }).error(function(response) {
-			console.log(response);
-		});
-	}
+    
+    // approve or reject existing booking 
+    $scope.updatePendingRequest = function(resourceId) {
+    	      	utilityFunctions.setResourceDetails(resourceId);
+    	       	$window.location.href = "pendingRequests.html";
+    	       	
+    }
 
 	$scope.fetchResourceDetails = function(resource) {
         //console.log(resource);
@@ -62,8 +42,8 @@ sidebarApp.controller('sidebarCtrl', function($scope, $http, $window, userDetail
         console.log(utilityFunctions.getResourceDetails());
 		$window.location.href = "resourceEdit.html";
 	}
-
-    $scope.newResource = function() {
+	
+	$scope.newResource = function() {
         utilityFunctions.setResourceDetails(null);
         $window.location.href = "resourceEdit.html";
     }
