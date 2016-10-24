27d658a4c34bae99b97137d8d721fbfacd0f7d2d
meta-vivek-mittal <vivek.mittal@metacube.com>
2016-10-24 11:54:39 +0530

    Add files via upload

--- src/main/webapp/ui/js/controller/inviteUserCtrl.js
+++ src/main/webapp/ui/js/controller/inviteUserCtrl.js
@@ -1,39 +1,52 @@
 // Created By - Arpit Pittie
-var inviteUserPage = angular.module('inviteUserApp', ['ui-notification', 'ngRoute', 'topbarApp', 'sidebarApp']);
+var inviteUserPage = angular.module('inviteUserApp', [ 'ui-notification',
+		'ngRoute', 'topbarApp', 'sidebarApp' ]);
 
 // Controlller for sending the invite to a user
-inviteUserPage.controller('inviteUserCtrl', function($scope, $http, $window, Notification) {
-    $scope.user = {};
+inviteUserPage
+		.controller(
+				'inviteUserCtrl',
+				function($scope, $http, $window, Notification) {
+					$scope.user = {};
 
-    $scope.sendInvite = function() {
-        $('#wrapper').hide();
-        $('#spinner').show();
+					$scope.sendInvite = function() {
+						$('#wrapper').hide();
+						$('#spinner').show();
 
-        // Request to send mail for Invitation
-        $http({
-            method: 'POST',
-            url: 'http://localhost:8080/Project-Authentication/user/sendInvitationToUser',
-            data: $scope.user,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).success(function(response) {
-            $('#wrapper').show();
-            $('#spinner').hide();
-            Notification.info({
-                message: "Invitation Send",
-                delay: 2000
-            });
-            setTimeout(function() {
-                $window.location.href = 'index.html';
-            }, 2500);
-        }).error(function(response) {
-            $('#wrapper').show();
-            $('#spinner').hide();
-            Notification.error({
-                message: "Couldn't establish connection",
-                delay: 2000
-            });
-        });
-    }
-});
+						// Request to send mail for Invitation
+						$http(
+								{
+									method : 'POST',
+									url : 'http://localhost:8080/Project-Authentication/user/sendInvitationToUser',
+									data : $scope.user,
+									headers : {
+										'Content-Type' : 'application/json'
+									}
+								}).success(function(response) {
+							$('#wrapper').show();
+							$('#spinner').hide();
+							if (response.status != 403) {
+								Notification.info({
+									message : "Invitation Send",
+									delay : 2000
+
+								});
+							} else {
+								Notification.info({
+									message : "User aleardy Exist",
+									delay : 2000
+								});
+							}
+							setTimeout(function() {
+								$window.location.href = 'index.html';
+							}, 2500);
+						}).error(function(response) {
+							$('#wrapper').show();
+							$('#spinner').hide();
+							Notification.error({
+								message : "Couldn't establish connection",
+								delay : 2000
+							});
+						});
+					}
+				});
